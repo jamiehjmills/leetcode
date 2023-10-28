c4df3098ba3b38a3781adc9cafffedce47bba2dc
Jamie Mills <hmills6@myune.edu.au>
2023-10-28 21:50:40 +1100

    solve the third question

--- ArraysAndString.py
+++ ArraysAndString.py
@@ -38,3 +38,33 @@ def find_target_array(s, target):
             return True
 
 find_target_array([1, 2, 4, 6, 8, 9, 14, 15] , 13)
+
+
+# Example 3: Given two sorted integer arrays arr1 and arr2,
+# return a new array that combines both of them and is also sorted.
+
+def combine(arr1, arr2):
+    # ans is the answer
+    ans = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            ans.append(arr1[i])
+            i += 1
+        else:
+            ans.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        ans.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        ans.append(arr2[j])
+        j += 1
+
+    return ans
+
+
+print(combine([2,5,7], [3,6,8,9]))
+
