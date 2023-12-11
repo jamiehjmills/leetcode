016632093dee515454b450cce7643f1082814792
Hyojoo Mills <hyojoo.mills@gmail.com>
2023-12-12 08:32:42 +1100

    sovle Example 1 for SlidingWindows.py

--- ArraysAndStrings/SlidingWindows.py
+++ ArraysAndStrings/SlidingWindows.py
@@ -1,52 +1,29 @@
 # Example 1: Given an array of positive integers nums and an integer k,
 # find the length of the longest subarray whose sum is less than or equal to k.
-# This is the problem we have been talking about above. We will now formally solve it.
 
-k = [3,1,2,7,4,2,1,1,5]
+nums = [3,1,2,7,4,2,1,1,5]
 
+def find_longest(nums, k):
 
-def find_longest(k):
     left = 0
-    right = 1
+    right = 0
+    curr = 0
+    answer = 0
 
-    target = 8
+    for index, item in enumerate(nums):
+        right = index
+        curr += nums[right]
+        while(curr > k):
+            curr -= nums[left]
+            left += 1
 
-    ans = 0
-    max = 0
-    sum = k[left] + k[right]
+        answer = max(answer, right-left+1)
 
-    while right < len(k):
-        if sum > target:
-            while left < right and sum > target:
-                sum -= k[left]
-                max -= 1
-                left += 1
-        else:
-            right += 1
-            sum += k[right]
-            max += 1
+    return answer
 
 
-### Java ###
+print(find_longest(nums, 4))
 
-# nums = [3,1,2,7,4,2,1,1,5]
 
-# public int findLength(int[] nums, int k) {
-#     int left = 0;
-#     int curr = 0; // curr is the current sum of the window
-#     int ans = 0;
-#
-#     for (int right = 0; right < nums.length; right++) {
-#         curr += nums[right];
-#         while (curr > k) {
-#             curr -= nums[left];
-#             left++;
-#         }
-#
-#         ans = Math.max(ans, right - left + 1);
-#     }
-#
-#     return ans;
-# }
 
-## TODO: copy the Java code above. 
+
