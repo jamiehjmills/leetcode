0eb7264b00ff0b306a97f782c41704d764cf776e
Hyojoo Mills <hyojoo.mills@gmail.com>
2024-08-03 16:32:10 +1000

    push it..

--- src/PowerBI/test.py
+++ src/PowerBI/test.py
@@ -6,25 +6,34 @@ def load_json_data(filename):
     with open(os.path.join('run_config_files', filename), 'r') as file:
         return json.load(file)
 
+def save_new_json_data(filename, data):
+    with open(os.path.join('run_config_files', filename), 'w') as file:
+        json.dump(data, file, indent=2)
+
 # Sample data for demonstration
 run_config_files = {
     'bank_fin_nifty': 'run_config_bank_fin_nifty.json',
     'bank_nifty': 'run_config_bank_nifty.json',
     'layout': 'run_config_bank.json',
-    'rule': 'rule.json'
+    'rule': 'rule.json',
+    'new': 'new.json'
 }
 
 layout_data=load_json_data(run_config_files['layout'])
 rule_data=load_json_data(run_config_files['rule'])
 #print(layout_data)
-print(rule_data)
+#print(rule_data)
 
-data_for_rendering = {}
+data_for_rendering = []
 for layout in layout_data:
-    print(layout["name"])
-    found_item = next(
-        (item for item in rule if item["name"] == layout["name"]), None
-    )
-    if found_item:
-        # TODO need to update here
+    #print(layout["name"])
+    for rule in rule_data:
+        if rule["name"] == layout["name"]:
+            data_for_rendering.append(rule)
+        else:
+            data_for_rendering.append(layout)
 
+
+#print(data_for_rendering)
+
+rule_data=save_new_json_data(run_config_files['new'],data_for_rendering)
